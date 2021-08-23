ba051d36e9c65f4dc3024454fc0a16c5dcb5b8a6
Esjay <esjayh@gmail.com>
2021-08-23 22:30:24 +0800

    organized files

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Esjay's Homepage<br/><br/>Coming soon... (if I find some time)
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
